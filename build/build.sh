204033ff58e7ae5e5150395b1cc13b8b955d7a82
edifus <edifus@codemin.us>
2025-05-31 19:20:37 -0600

    ghostty build dep, gtk4-layer-shell

--- build/build.sh
+++ build/build.sh
@@ -23,7 +23,7 @@ dnf5 install --enable-repo="copr:copr.fedorainfracloud.org:bazzite-org:bazzite" 
 # ghostty terminal
 log "Building ghostty terminal from source"
 workdir=$(pwd)
-dnf5 install -y blueprint-compiler gettext gtk4-devel libadwaita-devel zig
+dnf5 install -y blueprint-compiler gettext gtk4-devel gtk4-layer-shell libadwaita-devel zig
 git clone https://github.com/ghostty-org/ghostty
 cd ghostty && zig build -fsys=gtk4-layer-shell -p /usr -Doptimize=ReleaseFast
 cd $workdir && rm -rf ghostty
